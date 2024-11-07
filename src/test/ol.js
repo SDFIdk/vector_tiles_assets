01d53a2dadd162812f417e1512155a4ae89edda0
Johan Lund Neergaard <johanlundn@gmail.com>
2024-11-07 17:02:27 +0100

    Update ol.js

--- src/test/ol.js
+++ src/test/ol.js
@@ -1,14 +1,10 @@
 import Map from 'ol/Map'
 import View from 'ol/View'
+import MVT from 'ol/format/MVT'
 import { get as getProjection } from 'ol/proj'
 import { register } from 'ol/proj/proj4'
 import proj4 from 'proj4/dist/proj4'
 import { apply } from 'ol-mapbox-style'
-
-import TileLayer from 'ol/layer/Tile'
-import WMTS from 'ol/source/WMTS'
-import TileWMS from 'ol/source/TileWMS'
-import LayerGroup from 'ol/layer/Group'
 
 // get defaults from config
 const mapElement = document.getElementById('map')
@@ -21,6 +17,7 @@ register(proj4)
 const projection = getProjection(projConfig.name)
 projection.setExtent(projConfig.extent)
 
+const format = new MVT()
 // Custom setTileLoadFunction to add a header with a token
 const tileLoadFunctionWithTokenHeader = (tile, src) => {
   tile.setLoader((ext, res, proj) => {
@@ -64,25 +61,8 @@ const map = new Map({
   })
 })
 
-map.addLayer(new TileLayer({
-  opacity: 1.0,
-  visible: true,
-  source: new TileWMS({
-    crossOrigin: 'Anonymous',
-    url: 'https://api.dataforsyningen.dk/dagi_DAF?service=WMS&request=GetCapabilities&token=' + config.API_TOKEN,
-    params: {
-      'LAYERS': 'Kommuneinddeling',
-      'VERSION': '1.3.0',
-      'TRANSPARENT': 'TRUE'
-    }
-  })
-}))
-
-console.log(projConfig)
-
-const vectorLayerGroup = new LayerGroup()
 // Apply the style and add token header
-apply(vectorLayerGroup, stylefile, { resolutions: projConfig.resolutions, projection: projConfig.name })
+apply(map, stylefile, { resolutions: projConfig.resolutions, projection: projConfig.name })
 .then(layerGroup => {
   layerGroup.getLayers().forEach(layer => {
     const url = layer?.getSource()?.urls ? layer.getSource().urls[0] : ''
@@ -90,6 +70,4 @@ apply(vectorLayerGroup, stylefile, { resolutions: projConfig.resolutions, projec
       layer.getSource().setTileLoadFunction(tileLoadFunctionWithTokenHeader)
     }
   })
-  map.addLayer(layerGroup)
-  console.log(layerGroup.getLayers().getArray()[0].getSource())
 })
